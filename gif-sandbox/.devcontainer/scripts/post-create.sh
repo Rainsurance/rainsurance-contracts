cb31023103ce6efa6c5c0463fd2fc5a9627a28cf
André Teves <teves@webeleven.com.br>
2023-04-28 14:15:22 -0300

    etherisc gif sandbox initial commit

---
+++ gif-sandbox/.devcontainer/scripts/post-create.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+brownie networks add Local ganache host=http://ganache:7545 chainid=1234
+
+.devcontainer/scripts/deploy-gif.sh 
+
---
+++ gif-sandbox/server/policy.py
@@ -0,0 +1,10 @@
+from typing import Dict
+from pydantic import BaseModel
+
+class Policy(BaseModel):
+    id:str = None
+    object_name:str = None
+    request_id:int = None
+    premium:int = None
+    sum_insured:int = None
+    expired:bool = None
